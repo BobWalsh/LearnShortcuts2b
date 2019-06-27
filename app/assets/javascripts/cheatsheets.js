92b183899e709b76a9febc1038237f695e84b386
Bob Walsh <bob.walsh@47hats.com>
2019-06-27 10:33:25 -0700

    back at it

--- app/assets/javascripts/cheatsheets.js
+++ app/assets/javascripts/cheatsheets.js
@@ -7,22 +7,26 @@ $(document).ready(function () {
     .first()
     .data("page");
 
-  $(".card").flip({ axis: 'y' });
-  console.log("doc read cheatsheets");
-  if (this_page == "cheatsheets") {
-    // convert keycaps to actual keys
-    $(".interface-1").each(function (i, val) {
-      str = $(".interface-1").eq(i).text() + "";
-      str = str.trim();
-      // $("#interface-x").text(str);
-      temp_var = str;
-      keyarray = temp_var.split(", ");
-      temp_var2 = "";
-      $.each(keyarray, function (i, val) {
-        temp_var2 = temp_var2 + ConvertKeyCaps(val);
-      });
-      $(".interface-1").eq(i).text("");
-      $(".interface-1").eq(i).append(temp_var2);
-    });
-  };
+  // $(".card").flip({ axis: 'y' });
+  // console.log("doc read cheatsheets");
+  // if (this_page == "cheatsheets") {
+  //   // convert keycaps to actual keys
+  //   $(".interface-1").each(function (i, val) {
+  //     str = $(".interface-1").eq(i).text() + "";
+  //     str = str.trim();
+  //     // $("#interface-x").text(str);
+  //     temp_var = str;
+  //     keyarray = temp_var.split(", ");
+  //     temp_var2 = "";
+  //     $.each(keyarray, function (i, val) {
+  //       temp_var2 = temp_var2 + ConvertKeyCaps(val);
+  //     });
+  //     $(".interface-1").eq(i).text("");
+  //     $(".interface-1").eq(i).append(temp_var2);
+
+  //     // IDE
+  //     hljs.initHighlightingOnLoad();
+  hljs.initHighlighting();
+  //   });
+  // };
 });
